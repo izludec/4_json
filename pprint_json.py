b7711933de16aebb3904eb585bc4a3ed11855a7f
izludec <izludec@hotmail.com>
2017-02-16 20:14:16 +0300

    First commit

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,20 @@
 import json
+import os
 
 
 def load_data(filepath):
-    pass
+    file_with_json = open(filepath)
+    converted_json = json.loads(file_with_json.read())
+    return converted_json
 
 
-def pretty_print_json(data):
-    pass
+def pretty_print_json(json_data):
+    return json.dumps(
+        json_data, sort_keys=True, indent=4, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    pass
+    print(os.getcwd())
+    path_to_json = input("Enter path to JSON  \n")
+    json_data = load_data(path_to_json)
+    print(pretty_print_json(json_data))
