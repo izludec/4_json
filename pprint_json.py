65336330d68ceacc2264ad4e86c5998454d955d3
Kirill Izludec <izludec@hotmail.com>
2017-02-17 06:36:26 +0000

    Added exception handler

--- pprint_json.py
+++ pprint_json.py
@@ -3,17 +3,26 @@ import os
 
 
 def load_data(filepath):
-    file_with_json = open(filepath)
-    converted_json = json.loads(file_with_json.read())
-    return converted_json
+    if not os.path.exists(filepath):
+        print("File not found")
+        return None
+    with open(filepath, 'r') as file_handler:
+        try:
+            return json.load(file_handler)
+        except ValueError:
+            print("Wrong json")
+            return None
 
 
 def pretty_print_json(json_data):
-    return json.dumps(
-        json_data, sort_keys=True, indent=4, ensure_ascii=False)
+    pretty_print = None
+    pretty_print = json.dumps(
+    json_data, sort_keys=True, indent=4, ensure_ascii=False)
+    return pretty_print
 
 
 if __name__ == '__main__':
     path_to_json = input("Enter path to JSON  \n")
     json_data = load_data(path_to_json)
     print(pretty_print_json(json_data))
+
