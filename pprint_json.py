99d95d2d93b9f690dc5a71b6423c9c0cb189b2df
Kirill Izludec <izludec@hotmail.com>
2017-02-20 17:50:53 +0000

    Fix functional mistakes. Print functionality transfered to pretty_print_json. Exception intercept transfered to main function

--- pprint_json.py
+++ pprint_json.py
@@ -3,23 +3,24 @@ import os
 
 
 def load_data(filepath):
-    if not os.path.exists(filepath):
-        print("File not found")
-        return None
     with open(filepath, 'r') as file_handler:
-        try:
-            return json.load(file_handler)
-        except ValueError:
-            print("Wrong json")
-            return None
+        return json.load(file_handler)
 
 
 def pretty_print_json(json_data):
-    return json.dumps(
-    json_data, sort_keys=True, indent=4, ensure_ascii=False)
+    print(json.dumps(
+    json_data, sort_keys=True, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
     path_to_json = input("Enter path to JSON  \n")
-    json_data = load_data(path_to_json)
-    print(pretty_print_json(json_data))
+    try:
+        json_data = load_data(path_to_json)
+    except OSError:
+        print("File not found")
+        json_data = None
+    except ValueError:
+        print("Wrong json")
+        json_data = None
+    else:
+        pretty_print_json(json_data)
